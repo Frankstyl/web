c70763b0d6c8eca842aabaac33a3986f2dccaa92
Frankstyl <jfrankogramos@gmail.com>
2019-06-30 17:57:35 -0600

    adding a Handler

--- server/src/server.go
+++ server/src/server.go
@@ -2,17 +2,23 @@ package main
 
 import(
 		"net/http"
+		"fmt"
 )
 
 func hi(w http.ResponseWriter, r *http.Request){
 	 t:="hello"
 	 w.Write([]byte(t))
 }	
+func helloWorld(w http.ResponseWriter, r *http.Request){
+word:="hello World my name is Gopher"
+fmt.Fprintf(w,word)
+}
 
 
 func main(){
 	mux:=http.NewServeMux()
 	mux.HandleFunc("/hi",hi)
+	mux.HandleFunc("/hello",helloWorld)
 	
 	server:=&http.Server{
 		Addr: ":8080",
